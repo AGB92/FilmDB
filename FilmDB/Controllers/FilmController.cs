f962c8cc0e5a8aeb260f1de0145e896343b59da6
AGB92 <adam.geborek@gmail.com>
2024-05-19 20:39:37 +0200

    zajecia 3
    commit zmian na zajecia 3

--- FilmDB/Controllers/FilmController.cs
+++ FilmDB/Controllers/FilmController.cs
@@ -14,14 +14,42 @@ namespace FilmDB.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var film = new FilmModel()
-            {
-                Title = "Rambo",
-                Year = 1978
-            };
-
-            await _manager.AddFilmAsync(film);
+            //wywoania testowe
+                //var film = new FilmModel()
+                //{
+                //    // blad - nie mogę tak ale mamy to obsluzone ID = 2,
+                //    Title = "Mała Syrenka",
+                //    Year = 1990
+                //};
+                //await _manager.AddFilmAsync(film);
+                //await _manager.RemoveFilm(10);
+                //var filmByID = _manager.GetFilm(13);
+                //await _manager.ChangeTitle(13, "Rambo2");
+                //var filmByID = _manager.GetFilm(16);
+                //filmByID.Year = 2023;
+                //await _manager.UpdateFilm(filmByID);
+                //var films=_manager.GetFilms();
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(FilmModel film) 
+        {
+            try
+            {
+                _manager.AddFilmAsync(film);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return View(film);
+            }
+
         }
     }
 }
--- FilmDB/Repository/FilmManager.cs
+++ FilmDB/Repository/FilmManager.cs
@@ -13,24 +13,58 @@ namespace FilmDB.Repository
         }
         public async Task<bool> AddFilmAsync(FilmModel filmModel)
         {
-            await _context.AddAsync(filmModel);
-            await _context.SaveChangesAsync();
+            try 
+            {
+                await _context.AddAsync(filmModel);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e) 
+            {
+                filmModel.ID = 0;
+                await _context.SaveChangesAsync();
+            }
             return true;
         }
 
-        public FilmManager RemoveFilm(int id)
-        {return this;}
+        public async Task<FilmManager> RemoveFilm(int id)
+        {
+            var filmToDelete=_context.Films.SingleOrDefault(f=>f.ID == id);
+            if (filmToDelete != null)
+            {
+                _context.Films.Remove(filmToDelete);
+                await _context.SaveChangesAsync();
+            }
+            return this;
+        }
 
-        public FilmManager UpdateFilm(FilmModel filmModel)
-        {return this;}
+        public async Task<FilmManager> UpdateFilm(FilmModel filmModel)
+        {
+            _context.Films.Update(filmModel);
+            await _context.SaveChangesAsync();
+            return this;
+        }
 
-        public FilmManager ChangeTitle(int id, string newTitle)
-        {return this;}
+        public async Task<FilmManager> ChangeTitle(int id, string newTitle)
+        {
+            var filmToChangeTitle = GetFilm(id);
+            if(filmToChangeTitle != null)
+            {
+                filmToChangeTitle.Title = newTitle;
+                await _context.SaveChangesAsync();
+            }
+            return this;
+        }
 
-        public FilmManager GetFilm(int id)
-        {return null;}
+        public FilmModel GetFilm(int id)
+        {
+            var film = _context.Films.SingleOrDefault(f => f.ID == id);
+            return film;
+        }
 
         public List<FilmModel> GetFilms()
-        {return null;}
+        {
+            var films = _context.Films.ToList();
+            return films;
+        }
     }
 }
